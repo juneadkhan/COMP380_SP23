b65a004ff6c55bb87debaee1f3428ba887c4fd72
Junead Khan <54591708+juneadkhan@users.noreply.github.com>
2023-05-09 00:46:35 -0400

    final deploy i hope

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://juneadkhan.github.io/",
+  base: "/COMP380_SP23",
   integrations: [
     tailwind(),
     image({
